0e4d8cad8d42e954677ca57fad3363927c12fa01
Bhavin <icd.bhavin@gmail.com>
2022-06-27 15:01:24 +0530

    Add project files.

---
+++ WebAPI_Demo1_MongoDB/Controllers/BooksController.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * To learn about MongoDB with .NET Code Web API
+ * Learning References:
+ * https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mongo-app?view=aspnetcore-3.1&tabs=visual-studio
+ * 
+ */
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_Demo1_MongoDB.Services;
+
+namespace WebAPI_Demo1_MongoDB.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var result = _bookService.Get();
+            return Ok(result);
+        }
+    }
+}
---
+++ WebAPI_Demo1_MongoDB/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Demo1_MongoDB.Models
+{
+    public class BookStoreDatabaseSettings: IBookstoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollectionName { get; set; } = null!;
+    }
+    public interface IBookstoreDatabaseSettings
+    {
+        string BooksCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
